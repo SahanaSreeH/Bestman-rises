bc86ee3270bc89baf05a5bee67185e7749ebc5a8
SahanaSreeH <77058482+SahanaSreeH@users.noreply.github.com>
2021-04-13 00:00:19 +0530

    Add files via upload

---
+++ PRO-C42-template-main/Drop.js
@@ -0,0 +1,13 @@
+class Drop{
+    constructor(x, y){
+
+        var option = {
+            friction : 0.1
+        }
+
+        this.body = Bodies.circle(x, y, 5,options)
+    }
+    display(){
+        
+    }
+}
